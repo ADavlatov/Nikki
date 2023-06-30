5838297524236502918540b66510450eb4e10a39
ADavlatov <a@davlatov.space>
2023-06-30 18:27:20 +0300

    Add sign in
    Took 2 minutes

--- Nikki.Auth/Services/AuthService.cs
+++ Nikki.Auth/Services/AuthService.cs
@@ -1,6 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using Grpc.Core;
+using Nikki.Auth.Models;
+
 namespace Nikki.Auth.Services;
 
 public class AuthService
 {
-    
+    private readonly UserContext _db = new();
+
+    public override Task<SignInResponse> SignInUser(SignInRequest request, ServerCallContext context)
+    {
+        var result = RequestManager.ValidateSignInRequest(_db, request);
+
+        if (result != null)
+        {
+            return Task.FromResult(new SignInResponse { IsSucceed = false, Error = result });
+        }
+
+        _db.Users.Add(new User
+        {
+            Username = request.Username, Email = request.Email, Password = request.Password
+        });
+        _db.SaveChanges();
+
+        return Task.FromResult(new SignInResponse
+        {
+            IsSucceed = true,
+            AccessToken = new JwtSecurityTokenHandler().WriteToken(TokenService.GetJwtToken(request.Username, 1)),
+            RefreshToken =
+                new JwtSecurityTokenHandler().WriteToken(TokenService.GetJwtToken(request.Username, 15))
+        });
+    }
 }
--- Nikki.Auth/Services/RequestManager.cs
+++ Nikki.Auth/Services/RequestManager.cs
@@ -1,6 +1,19 @@
+using Nikki.Auth.Models;
+
 namespace Nikki.Auth.Services;
 
 public class RequestManager
 {
-    
+    public static string? ValidateSignInRequest(UserContext db, SignInRequest request)
+    {
+        User? user = db.Users.FirstOrDefault(x =>
+            (x.Username == request.Username || x.Email == request.Username) && x.Password == request.Password);
+
+        if (user == null)
+        {
+            return "Неверный логин или пароль";
+        }
+
+        return null;
+    }
 }
