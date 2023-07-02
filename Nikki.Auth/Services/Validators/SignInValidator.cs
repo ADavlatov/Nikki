bcf7d368d52371947f0189c1bfda278f363c5ee2
ADavlatov <a@davlatov.space>
2023-07-02 23:08:01 +0300

    Minor changes
    Took 2 minutes

--- Nikki.Auth/Services/Validators/SignInValidator.cs
+++ Nikki.Auth/Services/Validators/SignInValidator.cs
@@ -4,7 +4,7 @@ using Nikki.Auth.Models;
 
 namespace Nikki.Auth.Services.Validators;
 
-public class SignInValidator : AbstractValidator<User>
+public class SignInValidator : AbstractValidator<SignInRequest>
 {
     private UserContext _db = new();
 
@@ -12,7 +12,7 @@ public class SignInValidator : AbstractValidator<User>
     {
         RuleFor(x => x.Username).Must(x => x.All(char.IsLetter) || x.All(char.IsNumber))
             .WithMessage("Имя пользователя должно состоять только из букв и цифр.")
-            .Must(x => _db.Users.Any(y => y.Username == x))
+            .Must(x => !_db.Users.Any(y => y.Username == x))
             .WithMessage("Пользователь с таким именем уже существует.").MaximumLength(16)
             .WithMessage("Имя пользователя не должно превышать длину в 16 символов.").NotEmpty()
             .WithMessage("Введите имя пользователяю.");
