b9fb3e5e7ee4ba0319827de80b1b6e07f00df6fa
ADavlatov <a@davlatov.space>
2023-07-06 04:13:15 +0300

    Add error messages in log in validator
    Took 3 minutes

--- Nikki.Auth/Services/Validators/LogInValidator.cs
+++ Nikki.Auth/Services/Validators/LogInValidator.cs
@@ -6,11 +6,14 @@ namespace Nikki.Auth.Services.Validators;
 public class LogInValidator : AbstractValidator<LogInRequest>
 {
     private UserContext _db = new();
-    
+
     public LogInValidator()
     {
-        RuleFor(x => _db.Users.FirstOrDefault(y => (y.Username == x.Username || y.Email == x.Username) && y.Password == x.Password));
-        RuleFor(x => x.Username).Must(x => x.All(char.IsLetter) || x.All(char.IsNumber)).MaximumLength(16).NotEmpty();
-        RuleFor(x => x.Password).Must(x => x.All(char.IsLetter) || x.All(char.IsNumber)).MaximumLength(30).NotEmpty();
+        RuleFor(x => x)
+            .Must(x => _db.Users.FirstOrDefault(y =>
+                (y.Username == x.Username || y.Email == x.Username) && y.Password == x.Password) != null)
+            .WithMessage("Неверный логин или пароль").NotEmpty().WithMessage("Это поле обязательно для заполнения");
+        RuleFor(x => x.Username).NotEmpty().WithMessage("Это поле обязательно для заполнения");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Это поле обязательно для заполнения");
     }
 }
