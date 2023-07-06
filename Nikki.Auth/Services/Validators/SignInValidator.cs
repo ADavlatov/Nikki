b156073a70b995b92317dbb2dcf0d50d1dd4d018
ADavlatov <a@davlatov.space>
2023-07-06 19:45:07 +0300

    Minor changes
    Took 15 minutes

--- Nikki.Auth/Services/Validators/SignInValidator.cs
+++ Nikki.Auth/Services/Validators/SignInValidator.cs
@@ -13,14 +13,16 @@ public class SignInValidator : AbstractValidator<SignInRequest>
         RuleFor(x => x.Username).Must(x => x.All(char.IsLetter) || x.All(char.IsNumber))
             .WithMessage("Имя пользователя должно состоять только из букв и цифр.")
             .Must(x => !_db.Users.Any(y => y.Username == x))
-            .WithMessage("Пользователь с таким именем уже существует.").MaximumLength(16)
+            .WithMessage("Пользователь с таким именем уже существует.").MinimumLength(4)
+            .WithMessage("Имя пользователя должно состоять минимум из 4 символов").MaximumLength(16)
             .WithMessage("Имя пользователя не должно превышать длину в 16 символов.").NotEmpty()
             .WithMessage("Введите имя пользователяю.");
         RuleFor(x => x.Password).Must(x => x.All(char.IsLetter) || x.All(char.IsNumber))
-            .WithMessage("Пароль должен состоять только из букв и цифр.").MaximumLength(30)
+            .WithMessage("Пароль должен состоять только из букв и цифр.").MinimumLength(5)
+            .WithMessage("Пароль должен состоять минимум из 5 символов").MaximumLength(30)
             .WithMessage("Пароль не должен превышать длину в 16 символов.").NotEmpty()
             .WithMessage("Введите пароль.");
-        RuleFor(x => x.Email).Must(x => _db.Users.Any(y => y.Email == x))
+        RuleFor(x => x.Email).Must(x => !_db.Users.Any(y => y.Email == x))
             .WithMessage("Пользователь с таким адресом электронной почты уже существует.").NotEmpty()
             .EmailAddress(mode: EmailValidationMode.AspNetCoreCompatible)
             .WithMessage("Ошибка при вводе электронной почты.");
