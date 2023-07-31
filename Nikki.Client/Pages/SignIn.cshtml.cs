21dcaed1b24479fa25c5246c0bfe7a7e1c7857e7
ADavlatov <a@davlatov.space>
2023-07-31 03:58:59 +0300

    Add sign in on form submit

--- Nikki.Client/Pages/SignIn.cshtml.cs
+++ Nikki.Client/Pages/SignIn.cshtml.cs
@@ -1,11 +1,35 @@
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Nikki.Client.Pages;
 
+[IgnoreAntiforgeryToken]
 public class SignIn : PageModel
 {
+    public string[]? UsernameErrors { get; set; }
+    public string[]? EmailErrors { get; set; }
+    public string[]? PasswordErrors { get; set; }
     public void OnGet()
     {
         
+    }
+
+    public void OnPost(string username, string email, string password)
+    {
+        var client = new Auth.AuthClient(GrpcChannel.ForAddress("https://localhost:7269"));
+        var response = client.SignInUser(new SignInRequest { Username = username, Email = email, Password = password });
+
+        if (!response.IsSucceed)
+        {
+            UsernameErrors = response.UsernameErrors.Split(", ");
+            EmailErrors = response.EmailErrors.Split(", ");
+            PasswordErrors = response.PasswordErrors.Split(", ");
+        }
+        
+        Response.Cookies.Append("AccessToken", response.AccessToken);
+        Response.Cookies.Append("RefreshToken", response.RefreshToken);
+
+        RedirectToPage("Index");
     }
 }
