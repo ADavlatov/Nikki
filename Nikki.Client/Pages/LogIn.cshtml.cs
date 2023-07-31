3132e263d7888ed758af41b05c167bc193d7bbbd
ADavlatov <a@davlatov.space>
2023-07-31 03:59:25 +0300

    Change classes names and add lists of errors

--- Nikki.Client/Pages/LogIn.cshtml.cs
+++ Nikki.Client/Pages/LogIn.cshtml.cs
@@ -4,6 +4,8 @@ namespace Nikki.Client.Pages;
 
 public class LogIn : PageModel
 {
+    public string[]? UsernameErrors { get; set; }
+    public string[]? PasswordErrors { get; set; }
     public void OnGet()
     {
         
