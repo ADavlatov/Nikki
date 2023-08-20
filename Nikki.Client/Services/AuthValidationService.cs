9829bcb8d86898c91769ca016948744b11c91924
ADavlatov <a@davlatov.space>
2023-08-20 19:14:24 +0300

    Update auth validation service
    Took 1 hour 23 minutes

--- Nikki.Client/Services/AuthValidationService.cs
+++ Nikki.Client/Services/AuthValidationService.cs
@@ -1,4 +1,5 @@
 using Grpc.Net.Client;
+using Nikki.Auth;
 
 namespace Nikki.Client.Services;
 
@@ -6,35 +7,47 @@ public class AuthValidationService
 {
     public bool ValidateUserAuth(HttpRequest request, HttpResponse response)
     {
-        var client = new Auth.AuthClient(GrpcChannel.ForAddress(""));
-        string? accessToken;
-        
-        if (!request.Cookies.TryGetValue("AccessToken", out accessToken))
+        var client = new Auth.Auth.AuthClient(GrpcChannel.ForAddress("https://localhost:7269"));
+        string? refreshToken = null;
+
+        if (!request.Cookies.TryGetValue("AccessToken", out var accessToken) &&
+            !request.Cookies.TryGetValue("RefreshToken", out refreshToken))
         {
+            new LogoutService().Logout(response);
+
             return false;
         }
-        
-        var tokenValidationResponse = client.ValidateToken(new TokenValidationRequest { AccessToken = accessToken});
 
-        if (tokenValidationResponse.IsValid)
+        switch (accessToken)
         {
-            return true;
+            case null when refreshToken is not null:
+                var refreshResponse = client.GetAccessToken(new AccessTokenRequest { RefreshToken = refreshToken });
+
+                if (refreshResponse.IsValid)
+                {
+                    new TokenService().SetTokens(response, refreshResponse.AccessToken, refreshResponse.RefreshToken,
+                        refreshResponse.UserId);
+
+                    return true;
+                }
+
+                new LogoutService().Logout(response);
+
+                return false;
+            case not null when refreshToken is null:
+                new LogoutService().Logout(response);
+
+                return false;
+            case not null:
+                if (client.ValidateToken(new TokenValidationRequest { AccessToken = accessToken }).IsValid)
+                {
+                    return true;
+                }
+
+                return false;
         }
 
-        string? refreshToken;
-        
-        if (!request.Cookies.TryGetValue("RefreshToken", out refreshToken))
-        {
-            return false;
-        }
-        
-        var accessTokenResponse = client.GetAccessToken(new AccessTokenRequest { RefreshToken = refreshToken});
-
-        if (accessTokenResponse.IsValid)
-        {
-            new TokenService().SetTokens(response, accessTokenResponse.AccessToken, accessTokenResponse.RefreshToken);
-            new AuthValidationService().ValidateUserAuth(request, response);
-        }
+        new LogoutService().Logout(response);
 
         return false;
     }
