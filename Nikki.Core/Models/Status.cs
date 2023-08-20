07c48c045bb387e952ea67d3c748200b3bae2a97
ADavlatov <a@davlatov.space>
2023-08-21 01:57:42 +0300

    Minor changes
    Took 19 seconds

--- Nikki.Core/Models/Status.cs
+++ Nikki.Core/Models/Status.cs
@@ -5,6 +5,6 @@ public class Status
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Color { get; set; }
-    public List<Task> Tasks { get; set; }
+    public List<TaskModel> Tasks { get; set; }
     public Table Table { get; set; }
 }
