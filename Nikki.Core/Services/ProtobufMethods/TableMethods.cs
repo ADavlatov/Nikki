3c851c34457fa7290dc86b221a3913aed51f250c
ADavlatov <a@davlatov.space>
2023-08-22 12:14:48 +0300

    Add TableMethods
    Took 13 seconds

--- Nikki.Core/Services/ProtobufMethods/TableMethods.cs
+++ Nikki.Core/Services/ProtobufMethods/TableMethods.cs
@@ -1,14 +1,45 @@
+using Nikki.Core.Models;
+
 namespace Nikki.Core.Services.ProtobufMethods;
 
 public class TableMethods
 {
-    public AddTableResponse Add()
+    public AddTableResponse Add(AddTableRequest request, CoreContext db)
     {
-        return new AddTableResponse();
+        var tableValidator = new Validators.AddMethods.TableValidator().Validate(request);
+
+        if (!tableValidator.IsValid)
+        {
+            return new AddTableResponse { IsSucceed = false, Status = string.Join(", ", tableValidator.Errors) };
+        }
+
+        db.Tables!.Add(new Table
+        {
+            Name = request.Name,
+            Space = db.Spaces!.FirstOrDefault(x =>
+                x.Name == request.SpaceName && x.Id == request.SpaceId && x.UserId == request.UserId)!,
+            SpaceId = request.SpaceId, Statuses = new List<Status>()
+        });
+        db.SaveChanges();
+
+        return new AddTableResponse { IsSucceed = true, Status = "Ok" };
     }
 
-    public GetTablesResponse Get()
+    public GetTablesResponse Get(GetTablesRequest request, CoreContext db)
     {
-        return new GetTablesResponse();
+        var tableValidator = new Validators.GetMethods.TableValidator().Validate(request);
+
+        if (!tableValidator.IsValid)
+        {
+            return new GetTablesResponse { IsSucceed = false, Status = string.Join(", ", tableValidator.Errors) };
+        }
+
+        var tables = db.Tables;
+
+        return new GetTablesResponse
+        {
+            IsSucceed = true, TablesId = string.Join(", ", tables!.Select(x => x.Id)),
+            TablesName = string.Join(", ", tables!.Select(x => x.Name)), Status = "Ok"
+        };
     }
 }
