d8c3ec9fe5183c4fbcb27eb0ad795bd1e5333727
ADavlatov <a@davlatov.space>
2023-08-22 04:06:48 +0300

    Add SpaceMethods
    Took 12 seconds

--- Nikki.Core/Services/ProtobufMethods/SpaceMethods.cs
+++ Nikki.Core/Services/ProtobufMethods/SpaceMethods.cs
@@ -4,13 +4,38 @@ namespace Nikki.Core.Services.ProtobufMethods;
 
 public class SpaceMethods
 {
-    public AddSpaceResponse Add(AddSpaceRequest request, CoreContext? db)
+    public AddSpaceResponse Add(AddSpaceRequest request, CoreContext db)
     {
-        return new AddSpaceResponse();
+        var spaceValidator = new Validators.AddMethods.SpaceValidator().Validate(request);
+
+        if (spaceValidator.IsValid)
+        {
+            db.Spaces!.Add(new Space { Name = request.Name, UserId = request.UserId, Tables = new List<Table>() });
+            db.SaveChanges();
+
+            return new AddSpaceResponse { IsSucceed = true, Status = "Ok" };
+        }
+
+        return new AddSpaceResponse { IsSucceed = false, Status = string.Join(", ", spaceValidator.Errors) };
     }
 
-    public GetSpacesResponse Get()
+    public GetSpacesResponse Get(GetSpacesRequest request, CoreContext db)
     {
-        return new GetSpacesResponse();
+        var spaceValidator = new Validators.GetMethods.SpaceValidator().Validate(request);
+
+        if (spaceValidator.IsValid)
+        {
+            return new GetSpacesResponse
+            {
+                SpacesId = string.Join(", ", db.Spaces!.Select(x => x.Id)),
+                SpacesName = string.Join(", ", db.Spaces!.Select(x => x.Name)),
+                IsSucceed = true
+            };
+        }
+
+        return new GetSpacesResponse
+        {
+            IsSucceed = false, Status = string.Join(", ", spaceValidator.Errors)
+        };
     }
 }
