47fc37a94d7dd900950680989683c000e0c36f0c
ADavlatov <a@davlatov.space>
2023-08-22 04:07:11 +0300

    Minor changes
    Took 10 seconds

--- Nikki.Core/Services/CoreService.cs
+++ Nikki.Core/Services/CoreService.cs
@@ -1,38 +1,46 @@
 using Grpc.Core;
+using Nikki.Core.Models;
 using Nikki.Core.Services.ProtobufMethods;
 
 namespace Nikki.Core.Services;
 
 public class CoreService : Core.CoreBase
 {
+    private readonly CoreContext _db;
+
+    public CoreService(CoreContext db)
+    {
+        _db = db;
+    }
+
     public override Task<AddSpaceResponse> AddSpace(AddSpaceRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Space().Add());
+        return Task.FromResult(new SpaceMethods().Add(request, _db));
     }
 
     public override Task<GetSpacesResponse> GetSpaces(GetSpacesRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Space().Get());
+        return Task.FromResult(new SpaceMethods().Get(request, _db));
     }
 
     public override Task<AddTableResponse> AddTable(AddTableRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Table().Add());
+        return Task.FromResult(new TableMethods().Add());
     }
 
     public override Task<GetTablesResponse> GetTables(GetTablesRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Table().Get());
+        return Task.FromResult(new TableMethods().Get());
     }
 
     public override Task<AddStatusResponse> AddStatus(AddStatusRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new StatusMethods().Add());
+        return Task.FromResult(new StatusMethods().Add(request, _db));
     }
 
     public override Task<GetStatusesResponse> GetStatuses(GetStatusesRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new StatusMethods().Get());
+        return Task.FromResult(new StatusMethods().Get(request, _db));
     }
 
     public override Task<AddTaskResponse> AddTask(AddTaskRequest request, ServerCallContext context)
@@ -47,11 +55,11 @@ public class CoreService : Core.CoreBase
 
     public override Task<AddSubtaskResponse> AddSubtask(AddSubtaskRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Subtask().Add());
+        return Task.FromResult(new SubtaskMethods().Add());
     }
 
     public override Task<GetSubtasksResponse> GetSubtasks(GetSubtasksRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new Subtask().Get());
+        return Task.FromResult(new SubtaskMethods().Get());
     }
 }
